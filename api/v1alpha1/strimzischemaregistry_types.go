1e9872823c1f145c2019fa3b4570a7c280530329
Randsw <randsw@mail.ru>
2025-05-06 14:27:55 +0300

    feat: Add status field to CRD. Set to Ok if schema registry pod is running. Not ready otherwise

--- api/v1alpha1/strimzischemaregistry_types.go
+++ api/v1alpha1/strimzischemaregistry_types.go
@@ -41,6 +41,7 @@ type StrimziSchemaRegistrySpec struct {
 type StrimziSchemaRegistryStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Status string `json:"status"`
 }
 
 // +kubebuilder:object:root=true
